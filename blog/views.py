9bd75a9d51ae8a471aa19fafe5abedcf1b734241
Sparkowk - W <sparhowk@wp.pl>
2018-01-24 13:55:20 +0100

    co

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 3
     template_name = 'blog/post/list.html'
 
 
@@ -40,7 +40,25 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day
                              )
-    return render(request, template_name, {'post': post})
+    # List of active comments for this posta
+    comments = post.comments.filter(active=True)
+
+    if request.method == 'POST':
+        # A comments was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create comment object but dont save to database
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to comment
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    context = {'post': post,
+               'comments': comments,
+               'comment_form': comment_form}
+
+    return render(request, template_name, context)
     # return render(request, template_name, context)
 
 
@@ -52,9 +70,9 @@ def post_share(request, post_id):
     sent = False
 
     if request.method == 'POST':
-        form = EmailPostForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
+        email_form = EmailPostForm(request.POST)
+        if email_form.is_valid():
+            cd = email_form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = '{} ({}) zachęca do przeczytania "{}"'.format(
                 cd['name'], cd['email'], post.title)
